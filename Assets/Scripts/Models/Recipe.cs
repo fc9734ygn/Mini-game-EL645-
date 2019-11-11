790615a98f62185f7e006eddb39acf55065a3602
Mantas <latkauskas.mantas@gmail.com>
2019-11-11 13:37:28 +0000

    Added a few models

---
+++ Assets/Scripts/Models/Recipe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe
+{
+    public string title;
+    public List<Food> ingredients;
+}
