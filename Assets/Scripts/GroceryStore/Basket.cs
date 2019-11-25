17b0ea08b91774299dc63337ad9fd133f99275a0
Mantas <latkauskas.mantas@gmail.com>
2019-11-25 19:47:40 +0000

    Implemented basic mechanics in grocery store scene
    Disabled gravity for grocery prefabs, added background, added spawner, added finger-controlled basket (haven't done anything with collisions)

--- Assets/Scripts/GroceryStore/Basket.cs
+++ Assets/Scripts/GroceryStore/Basket.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        Screen.orientation = ScreenOrientation.Landscape;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+       
+        if (CheckIfSelected())
+        {
+            MoveBasket();
+        }
+    }
+
+    private void MoveBasket()
+    {
+        transform.position = new Vector3(GetPhoneCursorPosition().x, transform.position.y);
+    }
+
+    private bool CheckIfSelected()
+    {
+        Bounds basketBounds = GetComponent<SpriteRenderer>().bounds;
+      
+      //  Debug.Log("bounds  " + basketBounds.ToString() + "cursor pos   " + cursorPos.ToString() + "is in bounds   " + basketBounds.Contains(GetPhoneCursorPosition()));
+        return Input.touchCount == 1 && basketBounds.Contains(GetPhoneCursorPosition());
+    }
+
+
+    private Vector2 GetPhoneCursorPosition()
+    {
+       // Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        Vector2 pos = new Vector2(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+        return Camera.main.ScreenToWorldPoint(pos);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
     }
 }
---
+++ Assets/Scripts/GroceryStore/GrocerySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrocerySpawner : MonoBehaviour
+{
+    public GameObject onion, bacon, banana, carrot, detergent, oil, tomato, waterBottle;
+    private List<GameObject> prefabs;
+    public float spawnIntervalInSeconds; 
+    public float dropSpeed; 
+    public float maxX; 
+    public float minX; 
+
+    void Start()
+    {
+        //Start the spawn update
+        StartCoroutine("Spawn");
+        prefabs = new List<GameObject> { onion, bacon, banana, carrot, detergent, oil, tomato, waterBottle};
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(spawnIntervalInSeconds);
+
+        System.Random random = new System.Random();
+        int randomIndex = random.Next(prefabs.Count);
+        GameObject prefab = prefabs[randomIndex];
+
+
+        GameObject spawnedObject = Instantiate(prefab, new Vector3(Random.Range(minX, maxX), transform.position.y, 0f), Quaternion.Euler(0, 0, 0)) as GameObject;
+        spawnedObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -dropSpeed));
+
+        //Start the spawn again
+        StartCoroutine("Spawn");
+    }
+}
