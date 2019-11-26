4022f6a33432a6141a01dce78dc8053c5e6b7c91
Michail Skalistiris Pikis <michaelskalistirispikis@gmail.co>
2019-11-26 12:46:22 +0000

    Spawner Kitchen script update and addition of groceries

--- Assets/Scripts/Kitchen/Controllers/Spawn_items.cs
+++ Assets/Scripts/Kitchen/Controllers/Spawn_items.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class Spawn_items : MonoBehaviour
 {
-    public float spawnTime = 1; //Spawn Time
-    public GameObject apple; //Apple prefab
-    public GameObject bomb; //Bomb prefab
+    public float spawnTime= 1;
+
     public float upForce = 750; //Up force
     public float leftRightForce = 200; //Left and right force
-    public float maxX = -7; //Max x spawn position
-    public float minX = 7; //Min x spawn position
+    public float gravity = 1;
+    public float maxX = -10; //Max x spawn position
+    public float minX = 10; //Min x spawn position
+
+    private Object[] collectedGroceries;
+
     void Start()
     {
-        //Start the spawn update
+        collectedGroceries = Resources.LoadAll("Prefab/Groceries", typeof(GameObject));
         StartCoroutine("Spawn");
     }
+
     IEnumerator Spawn()
     {
         //Wait spawnTime
         yield return new WaitForSeconds(spawnTime);
         //Spawn prefab is apple
-        GameObject prefab = apple;
-        //If random is over 30
-        if (Random.Range(0, 100) < 30)
-        {
-            //Spawn prefab is bomb
-            prefab = bomb;
-            //you code here later in task 4
+        System.Random random = new System.Random();
+        int randomIndex = random.Next(collectedGroceries.Length - 1);
+        GameObject prefab = collectedGroceries[randomIndex] as GameObject;
+        prefab.GetComponent<Rigidbody2D>().gravityScale = gravity;
 
-        }
         //Spawn prefab add randomc position
         GameObject go = Instantiate(prefab, new Vector3(Random.Range(minX
         , maxX + 1), transform.position.y, 0f), Quaternion.Euler(0, 0, Random.Range(-
