445ab047b916ca0fbf56a304fc558758ef8338b0
Michail Skalistiris Pikis <michaelskalistirispikis@gmail.co>
2019-11-26 11:58:31 +0000

    Tool class, sprites and prefabs

---
+++ Assets/Scripts/Models/Tool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    public Tool.Type toolType;
+
+    public enum Type
+    {
+        Knife,
+        Mortar,
+        Grater,
+        Hand
+    }
+}
