42acb83836cb6ceb69c0d9cc01c60382c7a97489
Mantas <latkauskas.mantas@gmail.com>
2019-11-26 15:12:29 +0000

    Added healthbar (in progress), added sorting layers (z coord), assigned things to the layers, added script for healthbar

---
+++ Assets/Scripts/GroceryStore/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Sprite heartFull;
+    public Sprite hearthEmpty;
+
+    public GameObject heart0;
+    public GameObject heart1;
+    public GameObject heart2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
